0480ad9d0319f731d6a2bb58b0c5f132cb0e2033
MuhammadHanzala980 <muhammadhanzala980@gmail.com>
2021-04-13 00:17:24 +0500

    updated

--- database/index.js
+++ database/index.js
@@ -1,24 +1,32 @@
-let mongoose = require("mongoose");
+// let mongoose = require("mongoose");
 
+// mongoose.connect(
+//   "mongodb+srv://hanzala:anomno@cluster0.zwkdx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+//   {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   },
+//   (err) => {
+//     if (err) return console.log("Cannot connect to DB");
+//     console.log("DB is Connected");
+//     connectionCallback();
+//   }
+// );
+
+// let connectionCallback = () => {};
+
+// module.exports.onConnect = (callback) => {
+//   connectionCallback = callback;
+// };
 let BlockChainModel = require("./model");
 
-mongoose.connect(
-  "mongodb+srv://hanzala:anomno@cluster0.zwkdx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  },
-  (err) => {
-    if (err) return console.log("Cannot connect to DB");
-    console.log("DB is Connected");
-    connectionCallback();
-  }
-);
+var mongoose = require("mongoose");
+var uri = "mongodb+srv://hanzala:anomno@cluster0.zwkdx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-let connectionCallback = () => {};
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
-module.exports.onConnect = (callback) => {
-  connectionCallback = callback;
-};
-
-
+var db = mongoose.connection;
