5a48291fb73fb7099d0590635b484ea75d712dd0
MuhammadHanzala980 <muhammadhanzala980@gmail.com>
2021-04-12 23:54:39 +0500

    updated

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+require("dotenv/config");
 const express = require("express");
 let database = require("./database");
+
 const app = express();
 const postData = require("./Routes/post");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("server is running Now");
+});
 app.use("/app", postData);
 
-app.set("port", 9000);
+app.set("port", process.env.PORT || 9000);
 var server = app.listen(app.get("port"), function () {
   console.log("Express server listening on port " + server.address().port);
 });
-
-// var connection = require('./Connection/connection');
