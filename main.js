7ee39bcce057f4f7302068cba78e11d632ee2969
MuhammadHanzala980 <muhammadhanzala980@gmail.com>
2021-05-02 04:33:03 +0500

    updated

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const express = require("express");
 let database = require("./database");
 
 const app = express();
-const postData = require("./Routes/post");
+// const postData = require("./Routes/post");
 const cors = require("cors");
-const stripe = require("./Routes/stripe");
+// const stripe = require("./Routes/stripe");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,8 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Server is Running Now");
 });
-app.use("/app", postData);
-app.use("/payment", stripe);
+// app.use("/app", postData);
+// app.use("/payment", stripe);
 
 app.set("port", process.env.PORT || 9000);
 var server = app.listen(app.get("port"), function () {
